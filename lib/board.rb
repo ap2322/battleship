cf458ae437b62c191b5009df58353445fe526dfb
Zac Isaacson <isaacson.zm@gmail.com>
2019-08-21 13:59:33 -0600

    Create board with cells hash
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,22 @@
 class Board
-  attr_reader :cells
+  # attr_reader :cells
 
   def initialize
-    @cells = Hash.new()
-    @cells["A1"] = Cell.new("A1")
+    @cells = Hash.new
   end
 
+  def cells
+    keys = [
+      "A1", "A2", "A3", "A4",
+      "B1", "B2", "B3", "B4",
+      "C1", "C2", "C3", "C4",
+      "D1", "D2", "D3", "D4",
+    ]
+    keys.each do |key|
+      @cells[key] = Cell.new([key])
+    end
+    @cells
+  end
 
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -19,19 +19,16 @@ class BoardTest < Minitest::Test
     assert_instance_of Cell, @board.cells.values[0]
   end
 
-  def test_cell_keys_generation
-    # right now, just testing for a 4x4 board
-    @board.size(4,4)
-    #require 'pry'; binding.pry
+
+  def test_make_cells
     keys = [
       "A1", "A2", "A3", "A4",
       "B1", "B2", "B3", "B4",
       "C1", "C2", "C3", "C4",
       "D1", "D2", "D3", "D4"
     ]
-    # assert_equal keys, @board.size(4,4)
-    assert_equal 16, @board.size(4,4).length
-    # assert_equal keys, @board.cells.keys
-
+    assert_equal keys, @board.cells.keys
+    
   end
+
 end
