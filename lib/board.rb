2c3aeb63b1f61f8bd9430dabff886af76f2e72e3
Alice Post <ap2322@gmail.com>
2019-08-21 12:03:28 -0600

    More testing for setting up simple board

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,22 @@ class Board
 
   def initialize
     @cells = Hash.new()
+    @cells["A1"] = Cell.new("A1")
   end
 
+
+
 end
+
+
+# Think about this when refactoring for variable board size
+  # def size(y, x)
+  #   keys = []
+  #   y.times do |y|
+  #     # keys << y
+  #     x.times do |x|
+  #       keys << x
+  #     end
+  #   end
+  #   keys
+  # end
