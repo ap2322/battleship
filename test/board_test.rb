341ef86e5f01a94f4352e687f389a29af72c4b7d
Alice Post <ap2322@gmail.com>
2019-08-22 19:13:06 -0600

    One more test for overlap

--- test/board_test.rb
+++ test/board_test.rb
@@ -171,7 +171,9 @@ class BoardTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     @board.place(cruiser, ["A1", "A2", "A3"])
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, @board.valid_placement?(submarine, ["A1", "B1"])
+    assert_equal true, @board.valid_placement?(submarine, ["B1", "B2"])
   end
 
 end
