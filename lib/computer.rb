699fde418c7c327782f8dec951a4e2d2f316e785
Alice Post <ap2322@gmail.com>
2019-08-26 19:29:41 -0600

    Add computer tests and clean computer all_possible_placements method

--- lib/computer.rb
+++ lib/computer.rb
@@ -8,23 +8,20 @@ class Computer
 # Place a Ship and a Cruiser in random places on the board
 # These placements must be valid.
   def all_possible_placements(board, ship)
-    random_placement = []
+    all_random_placements = []
     board.cells.keys.each_cons(ship.length) do |key|
-      random_placement << key
+      all_random_placements << key
     end
-    random_placement
+    all_random_placements
   end
 
 
   def place_on_board(ship, board)
-    placed = false
     generated_placement = all_possible_placements(board, ship).sample
     until board.valid_placement?(ship, generated_placement) do
       generated_placement = all_possible_placements(board, ship).sample
     end
-    placed = true
     board.place(ship, generated_placement)
-    placed = true
   end
 
   def place_all_ships(board, ships)
